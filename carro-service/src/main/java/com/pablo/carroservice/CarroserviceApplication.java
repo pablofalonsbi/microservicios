73967c2af9dd937c64b928dc61f78ebdef2b3105
Pablo Falon <pablo.falon@sancristobal.com.ar>
2024-03-11 21:05:27 -0300

    eureka

--- carro-service/src/main/java/com/pablo/carroservice/CarroserviceApplication.java
+++ carro-service/src/main/java/com/pablo/carroservice/CarroserviceApplication.java
@@ -2,8 +2,10 @@ package com.pablo.carroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarroserviceApplication {
 
 	public static void main(String[] args) {
--- moto-service/src/main/java/com/pablo/motoservice/MotoserviceApplication.java
+++ moto-service/src/main/java/com/pablo/motoservice/MotoserviceApplication.java
@@ -2,8 +2,10 @@ package com.pablo.motoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotoserviceApplication {
 
 	public static void main(String[] args) {
